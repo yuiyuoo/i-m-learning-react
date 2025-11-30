c3cbb00145a8192df28d1340a0551dbd62b98f1b
yutranhi <vtranh6858@gmail.com>
2025-11-30 17:37:36 +0100

    make use of a map function to loop over the data array

--- chap2/travel-blog/src/components/Entry.jsx
+++ chap2/travel-blog/src/components/Entry.jsx
@@ -1,11 +1,11 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src="https://scrimba.com/links/travel-journal-japan-image-url" 
-                    alt="mount fuji" 
+                    src={props.img.src} 
+                    alt={props.img.alt} 
                 />
             </div>
             <div className="info-container">
@@ -14,11 +14,11 @@ export default function Entry() {
                     src="https://upload.wikimedia.org/wikipedia/commons/f/f2/678111-map-marker-512.png" 
                     alt="map marker icon"
                 />
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <span className="country">{props.country}</span>
+                <a href={props.ggMapsLink}>View on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
             
         </article>

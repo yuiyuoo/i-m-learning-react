bbe708825443c3e3f0fb474ab826f72e335f849c
yutranhi <vtranh6858@gmail.com>
2025-11-30 17:36:53 +0100

    add jokes-web

---
+++ chap2/jokes-web/src/App.jsx
@@ -0,0 +1,14 @@
+import Joke from "./Joke"
+import jokesData from "./jokesData"
+
+export default function App() {
+    const jokeElements = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
+    console.log(jokeElements)
+    return (
+        <main>
+            {jokeElements}
+        </main>
+    )
+}

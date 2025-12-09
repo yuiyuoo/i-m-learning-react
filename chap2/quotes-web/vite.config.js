913b6a20e82472f16b1686c67e2d51be93e7cff5
yutranhi <vtranh6858@gmail.com>
2025-12-09 13:23:22 +0100

    fix url

--- chap2/quotes-web/vite.config.js
+++ chap2/quotes-web/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'i-m-learning-react/quotes-web'
+  base: '/i-m-learning-react/quotes-web'
 })
